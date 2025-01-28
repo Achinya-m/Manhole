1cc033b95f06e1cd6db959c60da0933939f5c29a
achy <127174827+Achinya-m@users.noreply.github.com>
2025-01-28 13:05:28 +0700

    Update 2.3.5. Data table.sql

--- 2.3.5. Data table.sql
+++ 2.3.5. Data table.sql
@@ -1,18 +1,49 @@
 SELECT
-ROW_NUMBER() OVER (ORDER BY device ASC) AS `No.`,
-device AS Device,
-location AS Location,
-latitude AS Latitude,
-longitude AS Longtitude,
-commu AS Communication,
-commu_id AS 'Communication ID',
-manhole_distance AS 'Manhole Distance',
-offset_ultra_level AS 'Water Level Ultrasonic-Offset',
-Low_level,
-Medium_level,
-High_level,
-alert_decis AS 'Water Level Alert Decision',
-timestamp
-FROM mpete_manhole.setup
-ORDER BY device ASC
-LIMIT 1000000;
+  ROW_NUMBER() OVER (ORDER BY s.device ASC) AS `No.`,
+  s.device AS Device,
+  s.location AS Location,
+  s.latitude AS Latitude,
+  s.longitude AS Longitude,
+  s.commu AS Communication,
+  s.commu_id AS 'Communication ID',
+  s.manhole_distance AS 'Manhole Distance',
+  s.offset_ultra_level AS 'Water Level Ultrasonic-Offset',
+  d.Level2 AS "Manhole Cover Ultrasonic",
+  s.cover_setup AS "Manhole Cover Setup",
+  CASE
+        WHEN d.Level2 > s.cover_setup THEN 'OPEN !'
+        WHEN d.Level2 = 0 THEN 'OPEN !'
+        ELSE 'CLOSE'
+  END AS "Cover Status",
+
+  CONCAT(
+    s.High_level, 
+    ' % (', 
+    ROUND(s.manhole_distance * (s.High_level / 100), 2), 
+    ' cm)'
+  ) AS "High Level,",
+
+  CONCAT(
+    s.Medium_level, 
+    ' % (', 
+    ROUND(s.manhole_distance * (s.Medium_level / 100), 2), 
+    ' cm)'
+  ) AS "Medium Level,",
+
+  CONCAT(
+    s.Low_level, 
+    ' % (', 
+    ROUND(s.manhole_distance * (s.Low_level / 100), 2), 
+    ' cm)'
+  ) AS "Low Level,",
+
+
+  s.timestamp
+FROM mpete_manhole.setup s
+JOIN mpete_manhole.data d ON s.device = d.device  -- เชื่อมโยงข้อมูลจากตาราง data ตาม device
+WHERE d.timestamp = (
+    SELECT MAX(d2.timestamp)
+    FROM mpete_manhole.data d2
+    WHERE d2.device = d.device
+)
+ORDER BY d.device ASC;
