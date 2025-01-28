8c0de563b3ff7aace8cbc21a7d5b8e7204a5e367
achy <127174827+Achinya-m@users.noreply.github.com>
2025-01-28 13:00:09 +0700

    Update 2.1.3. Data table.sql

--- 2.1.3. Data table.sql
+++ 2.1.3. Data table.sql
@@ -1,22 +1,38 @@
 SELECT 
     ROW_NUMBER() OVER (ORDER BY d.device ASC) AS `No.`,
     d.device AS Device,
-    GREATEST(0, (s.manhole_distance - s.offset_ultra_level - d.level)) AS "Water Level",
-    CASE 
-        WHEN (s.manhole_distance - s.offset_ultra_level - d.Level) < 0 THEN 0
-        WHEN (s.manhole_distance - s.offset_ultra_level - d.Level) <= (s.manhole_distance * (s.Low_Level / 100)) THEN 1
-        WHEN (s.manhole_distance - s.offset_ultra_level - d.Level) <= (s.manhole_distance * (s.Medium_Level / 100)) THEN 2
-        WHEN (s.manhole_distance - s.offset_ultra_level - d.Level) <= (s.manhole_distance * (s.High_Level / 100)) THEN 3
-        WHEN (s.manhole_distance - s.offset_ultra_level - d.Level) > (s.manhole_distance * (s.High_Level / 100)) THEN 4
+
+    CONCAT(
+        '💧 ', 
+        GREATEST(0, (s.manhole_distance - s.offset_ultra_level - d.level)),
+        ' cm'
+    ) AS "Water Level",
+
+    CASE
+        WHEN (s.manhole_distance - s.offset_ultra_level - d.Level) <= 0 THEN 0
+        WHEN (s.manhole_distance - s.offset_ultra_level - d.Level) >= (s.manhole_distance * (s.High_Level / 100)) THEN 4
+        WHEN (s.manhole_distance - s.offset_ultra_level - d.Level) >= (s.manhole_distance * (s.Medium_Level / 100)) THEN 3
+        WHEN (s.manhole_distance - s.offset_ultra_level - d.Level) >= (s.manhole_distance * (s.Low_Level / 100)) THEN 2
+        WHEN (s.manhole_distance - s.offset_ultra_level - d.Level) < (s.manhole_distance * (s.Low_Level / 100)) THEN 1
         ELSE 0
     END AS "Water Level Status",
-    d.cover AS "Cover Status",
+
+    CASE
+            WHEN d.Level2 > s.cover_setup THEN 1
+            WHEN d.Level2 =0 THEN 1
+            ELSE 0
+    END AS "Cover Status",
     s.location AS Location,
     s.latitude AS Latitude,
     s.longitude AS Longitude,
     s.manhole_distance AS "Manhole Distance",
-    s.alert_decis AS "Water Level Alert Decision",
-    d.level AS "Ultrasonic Level",
+    CONCAT(
+        s.Low_Level, 
+        ' % (', 
+        ROUND(s.manhole_distance * (s.Low_Level / 100), 2), 
+        ' cm)'
+    ) AS "Water Level Alert",
+    d.level AS "Ultrasonic Level (Not Water)",
     d.timestamp AS Timestamp
 FROM mpete_manhole.data d
 JOIN mpete_manhole.setup s
