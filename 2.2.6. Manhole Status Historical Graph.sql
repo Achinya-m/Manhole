0caa8196fd6ac1af107ddda8255e5b68c29b2851
achy <127174827+Achinya-m@users.noreply.github.com>
2025-02-19 09:22:53 +0700

    Update 2.2.6. Manhole Status Historical Graph.sql

--- 2.2.6. Manhole Status Historical Graph.sql
+++ 2.2.6. Manhole Status Historical Graph.sql
@@ -1,6 +1,11 @@
 SELECT 
     d.device AS device_data,
-    d.cover AS "Value",
+    -- d.cover AS "Value",
+    CASE
+            WHEN d.Level2 > s.cover_setup THEN 1
+            WHEN d.Level2 =0 THEN 1
+            ELSE 0
+    END AS "Value",
     d.timestamp AS "time"
 FROM 
     mpete_manhole.data d
