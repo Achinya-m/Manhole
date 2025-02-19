595ffd5386f944bd486255afed75946b196addb5
achy <127174827+Achinya-m@users.noreply.github.com>
2025-02-19 09:23:55 +0700

    Update 2.3.4. Forms.sql

--- 2.3.4. Forms.sql
+++ 2.3.4. Forms.sql
@@ -1,16 +1,25 @@
 SELECT
   device,
   manhole_distance,
+  cover_setup,
   offset_ultra_level,
   Low_Level,
   Medium_Level,
   High_Level,
-  alert_decis,
   latitude,
   longitude,
   location,
   commu,
   commu_id,
+  "Only MWA admin can settings" AS Text,
+  CASE 
+        WHEN cover_ack = 1 THEN '🔕 Disable Line Notify [Cover Status]'
+        ELSE '🔔 Enable Line Notify'
+  END AS "cover ack",
+  CASE 
+        WHEN maintenance = 1 THEN '🔕 Disable Line Notify [Cover Status and Level Status]'
+        ELSE '🔔 Enable Line Notify'
+  END AS maintenance,
   timestamp
 FROM
   mpete_manhole.setup
