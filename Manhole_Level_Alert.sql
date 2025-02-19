b56e7675a75b26566803766765f219a3bcf3bd9e
achy <127174827+Achinya-m@users.noreply.github.com>
2025-02-19 09:35:34 +0700

    Update Manhole_Level_Alert.sql

--- Manhole_Level_Alert.sql
+++ Manhole_Level_Alert.sql
@@ -2,12 +2,15 @@ SELECT
     d.device AS Device,
     s.location AS Location,
     CONCAT(GREATEST(0, s.manhole_distance - s.offset_ultra_level - d.Level), ' cm') AS Level,
-    GREATEST(0, s.manhole_distance - s.offset_ultra_level - d.Level - (s.manhole_distance*(s.alert_decis/100))) AS Level_alert,
+    CASE 
+        WHEN GREATEST(0, s.manhole_distance - s.offset_ultra_level - d.Level ) > (s.manhole_distance*(s.Low_Level/100)) AND s.maintenance = 0 THEN 1 
+        ELSE 0 
+    END AS Level_alert,
     DATE_FORMAT(d.timestamp, '%Y-%m-%d %H:%i:%s') AS Timestamp,
     CASE
-        WHEN d.Cover = 0 THEN 'ฝาท่อปิด'
-        WHEN d.Cover = 1 THEN 'ฝาท่อเปิด'
-        ELSE 'ไม่ทราบสถานะ'
+        WHEN d.Level2 > s.cover_setup THEN 'ฝาท่อเปิด'
+        WHEN d.Level2 = 0 THEN 'ฝาท่อเปิด'
+        ELSE 'ฝาท่อปิด'
     END AS Cover_Status,
     CONCAT(
     -- Mapping เปอร์เซ็นต์แบตเตอรี่จาก Counter โดย ส่งทุกๆ 1 ชม. และแบตเตอรี่=0 เมื่อครบ 3 ปี : 24x365x3=26280
